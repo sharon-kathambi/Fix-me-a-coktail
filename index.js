592b0a690e07eefa17391f614ae8d8351f6fcdae
Sharon Mwenda <smkathambi@gmail.com>
2022-06-24 12:33:27 +0300

    add showdrink function

--- index.js
+++ index.js
@@ -1,32 +1,59 @@
-/*const URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a";
+//const URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a";
 
 document.addEventListener("DOMContentLoaded", ()=>{
-    presentDrinks(URL)
-})*/
+   showDrinks("https://www.thecocktaildb.com/api/json/v1/1/search.php?s") 
+})
+
+async function showDrinks(url) {
+    /*const data = await*/ fetchDrinks();
+    //console.log(data)
+    //fetch drinks
+    //display drinks
+   /*const section2 = await */displayDrinks();
+   // console.log(section2)
+    };
+
 
 function fetchDrinks() {
     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s")
     .then(response => response.json())
     .then(data => {
-       // console.log(data)
+      // console.log(data)
     displayDrinks(data)
 })
     .catch(error => console.log("Fetch error:", error))
 }
-fetchDrinks();
+//fetchDrinks();
 
 const section = document.getElementsByClassName(".section-center");
-    const title = document.getElementsByClassName(".title");
+const title = document.getElementsByClassName(".title");
 
-function displayDrinks({ drinks }){
-    //console.log(cocktail.drinks);
-    
-    if(!drinks){
+
+//const {idDrink:id,strDrink:name,strDrinkThumb:image} = drink
+function displayDrinks(cocktail){
+    //return "Hello world";
+    console.log(cocktail.drinks)
+} 
+   /* if(!drinks){
         //hide loading
         title.textContent = "Sorry, no drinks matched your search"
         section.innerHTML = null;
         return;
     }
+    const newDrinks = drinks.map((drink) =>{
+        //console.log(drink)
+        const {idDrink:id,strDrink:name,strDrinkThumb:image} = drink
+        return `<a href="#drinks_content">
+        <article class="cocktail" data-id="${id}">
+            <img src="${image}" alt="${name}"/>
+            <h4>${name}</h4>
+        </article>
+    </a>`
+    }).join("");
+    //hide loading
+   title.textContent = "";
+   section.innerHTML = newDrinks; 
+   return section;
 }
 console.log(displayDrinks());
 
